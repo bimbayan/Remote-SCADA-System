5573b16c1ef6bc6e98653809cf55659cd7115336
bimbayan <bimbayanchakraborty2014@gmail.com>
2026-07-02 15:25:46 +0530

    fix: import bug

--- production_app/src/recommendation_service.py
+++ production_app/src/recommendation_service.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pandas as pd
 
-from MTech_Project_Phase8.decision_engine import generate_decisions
+from decision_engine import generate_decisions
 
 from models import (
     PredictionResult,
